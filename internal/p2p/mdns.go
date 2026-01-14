ccf33b1209308da73e43cd177cad3a5a52903bae
akshay <akshayjha2111@gmail.com>
2026-01-14 10:08:38 +0530

    adding mdns config file

--- internal/p2p/mdns.go
+++ internal/p2p/mdns.go
@@ -1,1 +1,25 @@
 package p2p
+
+import (
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/p2p/discovery/mdns"
+)
+
+type discoveryNotifee struct{
+	PeerChan chan peer.AddrInfo
+}
+// interface to be called when new  peer is found
+func (n *discoveryNotifee)HandlePeerFound(pi peer.AddrInfo){
+	n.PeerChan <- pi
+}
+
+func InitMDNS(peerHost host.Host,rendezvous string)chan peer.AddrInfo{
+	n:=&discoveryNotifee{}
+	n.PeerChan=make(chan peer.AddrInfo)
+	ser:=mdns.NewMdnsService(peerHost,rendezvous,n)
+	if err := ser.Start(); err != nil {
+		panic(err)
+	}
+	return n.PeerChan
+}
